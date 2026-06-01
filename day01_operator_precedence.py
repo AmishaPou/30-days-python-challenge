4590d9d8b31d0b3a48ab282a54cfbec07017ed73
Amisha Paudel <amishapaudel457@gmail.com>
2026-06-01 14:10:25 +0545

    first commit

---
+++ day01_operator_precedence.py
@@ -0,0 +1,18 @@
+# Day 1 - Operator Precedence & Associativity
+
+print("Operator Precedence and Associativity\n")
+
+result1 = 2 + 3 * 4
+print("2 + 3 * 4 =", result1)
+
+result2 = 10 - 6 / 2
+print("10 - 6 / 2 =", result2)
+
+result3 = (2 + 3) * 4
+print("(2 + 3) * 4 =", result3)
+
+result4 = 20 / 5 * 2
+print("20 / 5 * 2 =", result4)
+
+result5 = 2 ** 3 ** 2
+print("2 ** 3 ** 2 =", result5)
